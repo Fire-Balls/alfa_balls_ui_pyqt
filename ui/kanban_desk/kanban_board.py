41dbe94ea6fcde09fcb52435c3eb18c1b1a93167
CatWithoutSoup <roma.malov2000@mail.ru>
2025-04-23 21:38:28 +0500

    Kanban fix outline, border, focus bugs

--- ui/kanban_desk/kanban_board.py
+++ ui/kanban_desk/kanban_board.py
@@ -1,17 +1,30 @@
+from PyQt6.QtCore import QEvent
 from PySide6.QtCore import QSize
-from PySide6.QtWidgets import QWidget, QHBoxLayout, QListWidget, QListWidgetItem, QVBoxLayout, QLabel
-from PySide6.QtCore import Qt
+from PySide6.QtGui import QPixmap, QIcon
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QListWidget, QListWidgetItem, QVBoxLayout, QLabel, QAbstractItemView
+from PySide6.QtCore import Qt, QEvent
+
+from ui.utils import get_resource_path
 
 
 class KanbanColumn(QListWidget):
-    def __init__(self, title, parent=None):
+    def __init__(self, title, board, parent=None):
         super().__init__(parent)
-        # Разрешение на передвижение
         self.setObjectName("kanban_desk")
+        # Разрешение на передвижение
         self.setAcceptDrops(True)
         self.setDragEnabled(True)
         self.setDefaultDropAction(Qt.MoveAction)
+        # Убрать пунктирное выделении при нажатии
+        self.setFocusPolicy(Qt.NoFocus)
+        # расстояние между тасками
         self.setSpacing(5)
+
+        #фикс бага при повторном выделении
+        self.board = board
+        self.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.itemSelectionChanged.connect(self.on_selection_changed)
+
         # Столбцы
         self.title_label = QLabel(title)
         self.title_label.setAlignment(Qt.AlignCenter)
@@ -26,15 +39,24 @@ class KanbanColumn(QListWidget):
     def widget(self):
         return self.container
 
+    def eventFilter(self, source, event):
+        if event.type() == QEvent.FocusOut:
+            self.clearSelection()  # Убираем выделение при потере фокуса
+        return super().eventFilter(source, event)
+
+    def on_selection_changed(self):
+        self.board.clear_all_selections(except_column=self)
+
 
 class KanbanBoard(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.task_counter = 0
         self.layout = QHBoxLayout(self)
         self.columns = {}
 
         for name in ["To Do", "In Progress", "Review", "Done"]:
-            column = KanbanColumn(name)
+            column = KanbanColumn(name, board=self)
             self.columns[name] = column
             self.layout.addWidget(column.widget())
 
@@ -43,8 +65,22 @@ class KanbanBoard(QWidget):
 
     def add_task(self, task_name, column_name):
         if column_name in self.columns:
-            item = QListWidgetItem(task_name)
-            item.setData(Qt.UserRole, "kanbanTask")
+            self.task_counter += 1
+            number = self.task_counter
 
-            item.setSizeHint(QSize(200, 40))
+            item = QListWidgetItem()
+            item.setText(f"{task_name}.{number}")
+            item.setData(Qt.UserRole, "kanbanTasks")
+            item.setTextAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+
+            # Установка круглого аватара
+            avatar_pixmap = QPixmap(get_resource_path("logo-alfabank.svg")).scaled(24, 24)
+            item.setIcon(QIcon(avatar_pixmap))
+            item.setSizeHint(QSize(200, 80))
+
             self.columns[column_name].addItem(item)
+
+    def clear_all_selections(self, except_column=None):
+        for column in self.columns.values():
+            if column is not except_column:
+                column.clearSelection()
